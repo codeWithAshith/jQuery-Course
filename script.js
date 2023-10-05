0068c6b505a4b58f19e7fec766bf203d9b31b60f
codewithashith <codewithashith@gmail.com>
2023-10-05 06:58:08 +0530

    Added form handling

--- script.js
+++ script.js
@@ -19,6 +19,35 @@ $(document).ready(function () {
   $("p:hidden").show(2000);
 });
 
+$(document).ready(function () {
+  $("#first_form").submit(function (e) {
+    e.preventDefault();
+    var first_name = $("#first_name").val();
+    var last_name = $("#last_name").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    $(".error").remove();
+
+    if (first_name.length < 1) {
+      $("#first_name").after(
+        '<span class="error">This field is required</span>'
+      );
+    }
+    if (last_name.length < 1) {
+      $("#last_name").after(
+        '<span class="error">This field is required</span>'
+      );
+    }
+
+    if (password.length < 8) {
+      $("#password").after(
+        '<span class="error">Password must be at least 8 characters long</span>'
+      );
+    }
+  });
+});
+
 // without ajax
 let xhttp = new XMLHttpRequest();
 
